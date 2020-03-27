59c9a5f26534a1c2169c4fc4bcdcf55d37b9101a
yzenith <yzenithcorp@gmail.com>
2020-03-27 14:12:36 -0500

    nodejs testing example

--- test/test-server.js
+++ test/test-server.js
@@ -150,3 +150,112 @@ describe("Shopping List", function() {
     );
   });
 });
+
+describe('Recipes', function(){
+  // Before our tests run, we activate the server. Our `runServer`
+  // function returns a promise, and we return the that promise by
+  // doing `return runServer`. If we didn't return a promise here,
+  // there's a possibility of a race condition where our tests start
+  // running before our server has started.
+  before(function() {
+    return runServer();
+  });
+
+  // although we only have one test module at the moment, we'll
+  // close our server at the end of these tests. Otherwise,
+  // if we add another test module that also has a `before` block
+  // that starts our server, it will cause an error because the
+  // server would still be running from the previous tests.
+  after(function() {
+    return closeServer();
+  });
+
+  // GET method testing
+  it("should list items on GET", function() {
+    // for Mocha tests, when we're dealing with asynchronous operations,
+    // we must either return a Promise object or else call a `done` callback
+    // at the end of the test. The `chai.request(server).get...` call is asynchronous
+    // and returns a Promise, so we just return it.
+    return chai
+      .request(app)
+      .get("/recipes")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("array");
+
+        // because we create three items on app load
+        expect(res.body.length).to.be.at.least(1);
+        // each item should be an object with key/value pairs
+        // for `id`, `name` and `ingredients`.
+        const expectedKeys = ["id", "name", "ingredients"];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a("object");
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  // POST method testing
+  it('should return update items', function(){
+    const newItem = {name: "new recipe", ingredients: ['onion', 'egg', 'water']};
+    return chai
+      .request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res){
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.include.keys("id", "name", "ingredients");
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(
+          Object.assign(newItem, {id: res.body.id}
+        ))
+      })
+  });
+
+  // PUT method testing
+  it('should update a recipe successfully', function(){
+    const recipe = {
+      name: "new recipe", 
+      ingredients: ["eggs", "water", "flower"]
+    };
+
+    return chai
+      .request(app)
+      .get('/recipes')
+      .then(function(res){
+        recipe.id = res.body[0].id;
+        return chai
+          .request(app)
+          .put(`/recipes/${recipe.id}`)
+          .send(recipe)
+      })
+      .then(function(res){
+        expect(res).to.have.status(204);
+        expect(res).to.be.a('object');
+        //expect(res.body).to.deep.equal(recipe);
+        //expect(res.body).to.include.keys(['id','name','ingredients']);
+        expect(res.body.id).to.not.equal(null)
+      })
+  })
+
+  // delete testing
+  it('should delete a recipe successfully', function(){
+
+    return chai
+      .request(app)
+      .get("/recipes")
+      .then(function(res){
+        let deleteID = res.body[0].id
+        return chai
+          .request(app)
+          .delete(`/recipes/${deleteID}`)
+      })
+      .then(function(res){
+        expect(res).to.have.status(204);
+      })
+  })
+
+})
